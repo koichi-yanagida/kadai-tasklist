944db5cf44532fdcc460a3bd798262421ad5f28c
koichi.yanagida <yamadataisuke59@gmail.com>
2019-12-04 14:08:58 +0000

    20191204 tasklistver.3.0

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,11 @@
 class TasksController < ApplicationController
-  
+  before_action :require_user_logged_in
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   def index
-      @tasks = Task.order(id: :desc).page(params[:page]).per(3)
+    if logged_in?
+      @task = current_user.tasks.build
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+    end
   end
 
   def show
@@ -13,12 +16,13 @@ class TasksController < ApplicationController
   end
 
   def create
-      @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
 
     if @task.save
       flash[:success] = 'タスクが正常に追加されました'
       redirect_to @task
     else
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
       flash.now[:danger] = 'タスクを追加できませんでした'
       render :new
     end
